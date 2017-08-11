e7ea3bbdfeded2bc5552aa890198a86d391a4fd5
aibin <1149299088@qq.com>
2017-08-11 15:25:35 +0800

    add

---
+++ work/j-ajax/router.js
@@ -0,0 +1,9 @@
+app.get('/loadMore', function (req, res) {
+    var curIndex = req.query.index;
+    var length = req.query.length;
+    var data = [];
+    for(var i = 0; i < length; i++) {
+        data.push('点开查看新闻' + (parseInt(curIndex) + i));
+    }
+    res.send(data);
+});
