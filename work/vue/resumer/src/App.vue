7b6c682d925b937262b340ab803bffbc255be1f4
aibin <1149299088@qq.com>
2017-09-05 15:50:26 +0800

    vue cli 生成框架 Topbar Editor Preview简单组件布局

---
+++ work/vue/resumer/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="page">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
+
+  import Topbar from './components/Topbar.vue';
+  import ResumeEditor from './components/ResumeEditor.vue';
+  import ResumePreview from './components/ResumePreview.vue';
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {}
+    },
+    components: {Topbar, ResumeEditor, ResumePreview}
+  }
+
+</script>
+
+<style>
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: #EAEBEC;
+  }
+
+  .page > main {
+    flex-grow: 1;
+  }
+
+  .page > main {
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+    width: 100%;
+    align-self: center;
+  }
+
+</style>
---
+++ work/vue/resumer/src/components/ResumePreview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="resumePreview">
+    I am resumePreview
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ResumePreview'
+  }
+</script>
+
+<style>
+  #resumePreview {
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #ffffff;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+  }
+</style>
